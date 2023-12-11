53977f32c5d322578b74d33a0702c990d340a0cf
M.Faiq Khokhar <108585447+Faiq-Khokhar@users.noreply.github.com>
2023-12-11 23:15:33 +0500

    Responsiveness added for navbar + forums page - DONE

--- e-ustaad-frontend/app/components/filters/Filters.js
+++ e-ustaad-frontend/app/components/filters/Filters.js
@@ -4,7 +4,7 @@ import { faFire } from "@fortawesome/free-solid-svg-icons";
 
 const Filters = ({ children }) => {
   return (
-    <div className="flex gap-2 px-3 py-1 border cursor-pointer rounded-2xl w-max hover:bg-primary hover:text-white">
+    <div className="flex gap-2 px-1 py-1 border cursor-pointer lg:px-3 rounded-2xl w-max hover:bg-primary hover:text-white">
     <FontAwesomeIcon icon={faFire} />
       <p className="text-sm">{children}</p>
     </div>
---
+++ e-ustaad-frontend/app/questions/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      I am Questions Page
+    </div>
+  )
+}
+
+export default page
